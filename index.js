6c8006d78cc838b076e3012e7b83b68c05b3b8ac
ARJUN <arjun1709@gmail.com>
2020-04-19 14:03:57 +0530

    added eslint rules.

--- index.js
+++ index.js
@@ -1,60 +1,70 @@
 const cryptojs = require('crypto-js');
 
-const { getRequest, postRequest, generateToken, sendTransaction } = require('./utils/helper')
-const { AUTH_SERVICE_URL } = require('./config')
+const {
+  getRequest, postRequest, generateToken, sendTransaction,
+} = require('./utils/helper');
+const { AUTH_SERVICE_URL } = require('./config');
 
 class PBTS {
-    constructor(authToken) {
-        this.authToken = authToken;
-    }
-    
-    async encryptedAndSavePrivateKey(payload) {
-        const { handlename, password, privateKey } = payload;
+  constructor(authToken) {
+    this.authToken = authToken;
+  }
 
-        var encryptedPrivateKey = cryptojs.AES.encrypt(privateKey, password)
-        const encryptedPrivateKeyString = encryptedPrivateKey.toString();
+  async encryptedAndSavePrivateKey(payload) {
+    const { handlename, password, privateKey } = payload;
 
-        const params = { encryptedPrivateKey: encryptedPrivateKeyString };
+    const encryptedPrivateKey = cryptojs.AES.encrypt(privateKey, password);
+    const encryptedPrivateKeyString = encryptedPrivateKey.toString();
 
-        const { response: accessToken, error } = await generateToken({ handlename, password, authToken: this.authToken });
+    const params = { encryptedPrivateKey: encryptedPrivateKeyString };
 
-        if (error) {
-            return error;
-        }
+    const { response: accessToken, error } = await generateToken({ handlename, password, authToken: this.authToken });
 
-        const { response } = await postRequest({ params, url: `${AUTH_SERVICE_URL}/auth/private-key`, authToken: this.authToken, accessToken });
-        
-        if (response) {
-            return response;
-        }
-        return "There has been an issue. Pleaser try again later."
+    if (error) {
+      return error;
     }
 
-    async decryptAndSignTransaction(payload) {
-        const { password, handlename, rawTx, infuraKey, rpcUrl } = payload;
+    const { response } = await postRequest({
+      params, url: `${AUTH_SERVICE_URL}/auth/private-key`, authToken: this.authToken, accessToken,
+    });
 
-        const { response: accessToken, error } = await generateToken({ handlename, password, authToken: this.authToken });
-
-        if (error) {
-            return error;
-        }
-
-        const { data } = await getRequest({ url: `${AUTH_SERVICE_URL}/auth/private-key`, authToken: this.authToken, accessToken })
-
-        if (data) {
-            var bytes = cryptojs.AES.decrypt(data.data.encryptedPrivateKey, password);
-            var privateKey = bytes.toString(cryptojs.enc.Utf8);
-
-            const { response, error } = await sendTransaction({ privateKey, rawTx, infuraKey, rpcUrl });
-
-            if (error) {
-                return error;
-            }
-
-            return response;
-        }
-        return "Error occured. Please try again."
+    if (response) {
+      return response;
     }
+
+    return 'There has been an issue. Pleaser try again later.';
+  }
+
+  async decryptAndSignTransaction(payload) {
+    const {
+      password, handlename, rawTx, infuraKey, rpcUrl,
+    } = payload;
+
+    const { response: accessToken, error } = await generateToken({ handlename, password, authToken: this.authToken });
+
+    if (error) {
+      return error;
+    }
+
+    const { data } = await getRequest({ url: `${AUTH_SERVICE_URL}/auth/private-key`, authToken: this.authToken, accessToken });
+
+    if (data) {
+      const bytes = cryptojs.AES.decrypt(data.data.encryptedPrivateKey, password);
+      const privateKey = bytes.toString(cryptojs.enc.Utf8);
+
+      const { response, error: err } = await sendTransaction({
+        privateKey, rawTx, infuraKey, rpcUrl,
+      });
+
+      if (err) {
+        return err;
+      }
+
+      return response;
+    }
+
+    return 'Error occured. Please try again.';
+  }
 }
 
 module.exports = PBTS;
