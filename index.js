6b839240a76fee93181f4254d7fe6682f2513723
arjun1709@bitbucket.org <arjun1709@gmail.com>
2020-05-09 18:49:55 +0530

    added decrypt pvt key inside signKey function.

--- index.js
+++ index.js
@@ -14,33 +14,33 @@ async function AccessToken({ handlename, password, authToken }) {
     return { accessToken: response }
 }
 
+async function encryptKey({ privateKey, password }) {
+    const encryptedPrivateKey = cryptojs.AES.encrypt(privateKey, password)
+    const encryptedPrivateKeyString = encryptedPrivateKey.toString();
+    return { encryptedPrivateKeyString };
+}
+
+async function decryptKey({ encryptedPrivateKey, password }) {
+    const bytes = cryptojs.AES.decrypt(encryptedPrivateKey, password);
+    const privateKey = bytes.toString(cryptojs.enc.Utf8);
+
+    if (privateKey === "") {
+        return { error: 'Wrong password.' }
+    }
+    return { privateKey };
+}
+
+async function getKey() {
+    const { data } = await getRequest({ url: `${AUTH_SERVICE_URL}/auth/private-key`, authToken: this.authToken })
+    if (data) {
+        return { encryptedPrivateKey: data.data.encryptedPrivateKey }
+    }
+    return { error: "Error occured. Please try again." }
+}
+
 class PBTS {
     constructor(authToken) {
         this.authToken = authToken;
-    }
-
-    async encryptKey({ privateKey, password }) {
-        const encryptedPrivateKey = cryptojs.AES.encrypt(privateKey, password)
-        const encryptedPrivateKeyString = encryptedPrivateKey.toString();
-        return { encryptedPrivateKeyString };
-    }
-
-    async decryptKey({ encryptedPrivateKey, password }) {
-        const bytes = cryptojs.AES.decrypt(encryptedPrivateKey, password);
-        const privateKey = bytes.toString(cryptojs.enc.Utf8);
-
-        if (privateKey === "") {
-            return { error: 'Wrong password.' }
-        }
-        return { privateKey };
-    }
-
-    async getKey() {
-        const { data } = await getRequest({ url: `${AUTH_SERVICE_URL}/auth/private-key`, authToken: this.authToken })
-        if (data) {
-            return { encryptedPrivateKey: data.data.encryptedPrivateKey }
-        }
-        return { error: "Error occured. Please try again." }
     }
 
     async postKey({ encryptedPrivateKey, accessToken }) {
@@ -60,9 +60,9 @@ class PBTS {
             return { error };
         }
 
-        const encryptedPrivateKey = await this.encryptKey({ privateKey, password });
+        const encryptedPrivateKey = await encryptKey({ privateKey, password });
 
-        const { response, error: err } = await this.postKey({ encryptedPrivateKey, accessToken });
+        const { response, error: err } = await postKey({ encryptedPrivateKey, accessToken });
 
         if (err) {
             return { err };
@@ -70,7 +70,9 @@ class PBTS {
         return { response }
     }
 
-    async signKey({ privateKey, infuraKey, rpcUrl, rawTx }) {
+    async signKey({ encryptedPrivateKey, infuraKey, rpcUrl, rawTx }) {
+        const privateKey = await decryptKey({ encryptedPrivateKey, password })
+
         const { response, error } = await sendTransaction({ privateKey, rawTx, infuraKey, rpcUrl });
         
         if (error) {
