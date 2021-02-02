ab4f5c2e2ad937ce90d3e22f3c4f368ae8a57d6d
Arjun Sasikumar <arjun@inblox.me>
2021-02-03 01:51:06 +0530

    Updated the env vars and created a method to get the base URL.

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,13 @@
 module.exports = {
-  AUTH_SERVICE_URL: 'https://test-auth.inblox.me',
-  RELAYER_SERVICE_URL: 'https://test-relayer.inblox.me',
-  INFURA_KEY: 'c52ef01cba8b4e1d8aa98c12c64318a9',
+  AUTH_SERVICE_URL_TEST: 'https://test-auth.inblox.me',
+  AUTH_SERVICE_URL_DEV: 'https://dev-auth.inblox.me',
+  AUTH_SERVICE_URL_PROD: 'https://auth.inblox.me',
+  RELAYER_SERVICE_URL_TEST: 'https://test-relayer.inblox.me',
+  RELAYER_SERVICE_URL_DEV: 'https://dev-relayer.inblox.me',
+  RELAYER_SERVICE_URL_PROD: 'https://relayer.inblox.me',
+  RPC_URL_ROPSTEN: 'https://ropsten.infura.io/v3/c52ef01cba8b4e1d8aa98c12c64318a9',
+  RPC_URL_RINKEBY: 'https://rinkeby.infura.io/v3/c52ef01cba8b4e1d8aa98c12c64318a9',
+  RPC_URL_KOVAN: 'https://kovan.infura.io/v3/c52ef01cba8b4e1d8aa98c12c64318a9',
+  RPC_URL_GOERLI: 'https://goerli.infura.io/v3/c52ef01cba8b4e1d8aa98c12c64318a9',
+  RPC_URL_MAINNET: 'https://mainnet.infura.io/v3/c52ef01cba8b4e1d8aa98c12c64318a9',
 };
